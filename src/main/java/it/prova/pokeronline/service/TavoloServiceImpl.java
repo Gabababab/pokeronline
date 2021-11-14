bae6ae528d78c061a0462b27303502532e776842
Gabababab <gravottagabriele@gmail.com>
2021-11-14 19:58:04 +0100

    implementato inserimento di un nuovo tavolo

--- src/main/java/it/prova/pokeronline/service/TavoloServiceImpl.java
+++ src/main/java/it/prova/pokeronline/service/TavoloServiceImpl.java
@@ -1,5 +1,6 @@
 package it.prova.pokeronline.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,7 @@ public class TavoloServiceImpl implements TavoloService{
 
 	@Transactional
 	public void inserisciNuovo(Tavolo tavoloInstance) {
+		tavoloInstance.setDateCreated(new Date());
 		repository.save(tavoloInstance);
 	}
 
--- src/main/java/it/prova/pokeronline/web/controller/TavoloController.java
+++ src/main/java/it/prova/pokeronline/web/controller/TavoloController.java
@@ -1,22 +1,29 @@
 package it.prova.pokeronline.web.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.prova.pokeronline.service.TavoloService;
 import it.prova.pokeronline.service.UtenteService;
 import it.prova.pokeronline.dto.TavoloDTO;
+import it.prova.pokeronline.dto.UtenteDTO;
 import it.prova.pokeronline.model.Tavolo;
+import it.prova.pokeronline.model.Utente;
 
 @Controller
 @RequestMapping(value = "/tavolo")
@@ -55,5 +62,30 @@ public class TavoloController {
 	public String searchTavolo(Model model) {
 		model.addAttribute("search_tavolo_attr", new TavoloDTO());
 		return "tavolo/search";
+	}
+	
+	@GetMapping("/insert")
+	public String createTavolo(Model model) {
+		model.addAttribute("insert_tavolo_attr", new TavoloDTO());
+		return "tavolo/insert";
+	}
+
+	@PostMapping("/save")
+	public String saveTavolo(@Valid @ModelAttribute("insert_tavolo_attr") TavoloDTO tavoloDTO, BindingResult result,
+			RedirectAttributes redirectAttrs, HttpServletRequest request, Principal principal) {
+
+
+		if (result.hasErrors()) 
+			return "tavolo/insert";
+		
+		Utente utenteInserimento= utenteService.findByUsername(principal.getName());
+		UtenteDTO utenteCreatore = new UtenteDTO();
+		utenteCreatore.setId(utenteInserimento.getId());
+		tavoloDTO.setUtenteCreatore(utenteCreatore);
+	
+		tavoloService.inserisciNuovo(tavoloDTO.buildTavoloModel());
+
+		redirectAttrs.addFlashAttribute("successMessage", "Operazione eseguita correttamente");
+		return "redirect:/tavolo/searchTavoliUtente";
 	}
 }
